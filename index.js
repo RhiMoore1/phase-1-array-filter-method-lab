87f6cf61fa5b1e82a61251e29e27835a078c84a8
Rhiannon Moore <rhiannonmoore10@yahoo.com>
2023-03-25 15:43:26 -0700

    completes lab

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 // Code your solution here
+function findMatching(drivers, string) {
+    return drivers.filter(function(driver) {
+        return driver.toUpperCase() === string.toUpperCase()
+    })
+}
+
+function fuzzyMatch(drivers, string) {
+    return drivers.filter(function(driver) {
+        return driver.slice(0, 1) === string.slice(0, 1);
+    })
+}
+
+function matchName(drivers, string) {
+    return drivers.filter(function(driver) {
+        return driver.name === string;
+    })
+}
